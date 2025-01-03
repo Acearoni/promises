934211b52715497fafb6d0bd07ebe6e4adefcd35
Acearoni <awoodson.contact@gmail.com>
2025-01-02 21:21:53 -0600

    API change to first three starters and evolutions, displaying the data within the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
 import './App.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
+
+
 function App() {
+  const [pokeList, setPokeList] = useState([])
+  const [err, setErr] = useState('')
+
 
   // useEffect(() => {},[]) skeleton of a useEffect hook
   useEffect(() => {
     //API Call, runs when component is mounted.
     // console.log('Component is mounted');
-    axios.get('https://pokeapi.co/api/v2/pokemon/mewtwo')
-      .then((response) => console.log('RESPONSE: ',response))
-      .catch((error) => console.log(error))
-  }, []) //[] = Dependency Array, what you want useEffect hook to monitor.
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0')
+      .then((response) => {
+        console.log('RESPONSE:', response.data.results)
+        setPokeList(response.data.results)
+      })
+      .catch((error) => {
+        console.log('ERROR', error)
+        setErr('Something Went Wrong')
+      })
+  }, []) //[] = Dependency Array, what you want useEffect hook to monitor. Runs once, once you reload the page
 
 
   return (
     <>
-      <h2>Promises Intro</h2>
+      <h1>Pokemon API</h1>
+
+      <h2>First Starters and Their Evolutions</h2>
+      {
+        err.length > 0 ?
+          <p>{err}</p> :
+          null
+      }
+      {
+        pokeList.map((poke, idx) => (
+          <div key={idx}>
+            <h3>Pokemon Name: {poke.name}</h3>
+          </div>
+        ))
+      }
+
     </>
   )
 }
